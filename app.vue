65bada94f3dfc8e7e6b19fcce64f5404a3a87417
Duy <duylqh@adgad-net.vn>
2022-10-12 13:16:21 +0700

    auto import

--- app.vue
+++ app.vue
@@ -1,5 +1,19 @@
 <template>
   <div>
-    <NuxtWelcome />
-  </div>
+    <div>Hello World</div>
+    <button @click="increment">Increment</button>
+    <p>{{ count }}</p>
+    <button @click="decrement">Decrement</button>
+</div>
 </template>
+
+<script setup>
+  const count = ref(0);
+function increment() {
+  count.value++;
+}
+
+function decrement() {
+  count.value--;
+}
+</script>
