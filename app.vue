d83fffe5a38ce8052b8a64885957e91d93e09f14
Duy <duylqh@adgad-net.vn>
2022-10-12 13:28:56 +0700

    Component import

--- app.vue
+++ app.vue
@@ -1,19 +1,5 @@
 <template>
-  <div>
-    <div>Hello World</div>
-    <button @click="increment">Increment</button>
-    <p>{{ count }}</p>
-    <button @click="decrement">Decrement</button>
-</div>
+  <HomeWelcome />
+  <Counter />
 </template>
 
-<script setup>
-  const count = ref(0);
-function increment() {
-  count.value++;
-}
-
-function decrement() {
-  count.value--;
-}
-</script>
